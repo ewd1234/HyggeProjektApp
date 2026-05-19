5334cb20df2a1c04c29a43931b8ccec7d666ebf3
jacobPC <jacoblorenzen02@gmail.com>
2026-05-19 15:33:26 +0200

    Add project files.

---
+++ Core/Models/Player.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Models
+{
+    public class Player
+    {
+        public string Name { get; set; } = "";
+        public string Color { get; set; } = "#ffffff"; // Standard hvid
+        public int Position { get; set; } = 0; // Alle starter på 0 (START)
+    }
+}
---
+++ Core/Models/Tile.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Models
+{
+    public class Tile
+    {
+        public int Id { get; set; }          // Feltets nummer (1, 2, 3...)
+        public string Title { get; set; } = ""; // Overskrift (f.eks. "3 SUG")
+        public string Description { get; set; } = ""; // Ekstra info (f.eks. "12 SEK")
+        public string Color { get; set; } = "#ffffff"; // Baggrundsfarve
+        public TileType Type { get; set; } = TileType.Normal;
+    }
+
+    public enum TileType
+    {
+        Normal,
+        Start,
+        Slut,
+        Special // Bruges til f.eks. "Slå tilbage" eller "Ninja" ikonerne
+    }
+}
